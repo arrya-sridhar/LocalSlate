5a6436f8017b9b23e1431eebb6285ac65857b3b4
Sreeshanth <sreeshanthmunupally@gmail.com>
2026-06-30 13:49:02 +0530

    Fix CI pipeline

--- backend/src/database/db.py
+++ backend/src/database/db.py
@@ -263,9 +263,11 @@ class DatabaseService:
                 system_summary=incident.system_summary,
                 incident_type=incident.incident_type,
                 location=incident.location,
-                affected_systems=", ".join(incident.affected_systems)
-                if incident.affected_systems
-                else None,
+                affected_systems=(
+                    ", ".join(incident.affected_systems)
+                    if incident.affected_systems
+                    else None
+                ),
             )
 
             for loc in incident.identified_entities.locations:
@@ -328,11 +330,11 @@ class DatabaseService:
                         ],
                         "incident_type": inc.incident_type,
                         "location": inc.location,
-                        "affected_systems": [
-                            sys.strip() for sys in inc.affected_systems.split(",")
-                        ]
-                        if inc.affected_systems
-                        else [],
+                        "affected_systems": (
+                            [sys.strip() for sys in inc.affected_systems.split(",")]
+                            if inc.affected_systems
+                            else []
+                        ),
                     }
                 )
             return incidents
